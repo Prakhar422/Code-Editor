7c6cb32e97b46de1c2649316816973e7564a51c4
Prakhar garg <gargprakhar422@gmail.com>
2026-05-19 17:26:25 +0530

    First Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,36 +1,23 @@
 import React from "react";
 import { AuroraText } from "./components/AuroraText";
+import { Editor } from "@monaco-editor/react";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen overflow-hidden relative bg-black text-white flex flex-col">
+   <main className="h-screen w-full bg-gray-950 flex gap-4 p-2">
 
-      {/* CONTENT */}
-        {/* HERO */}
-        <main className="flex flex-1 flex-col items-center justify-center text-center px-4">
-          {/* BADGE */}
-          <div className="rounded-full border border-white/10 bg-white/5 px-4 py-1 text-sm backdrop-blur">
-            Thankyou for using
-          </div>
+    <aside className="h-full w-1/4 bg-amber-50 rounded-lg">
+    </aside>
+    <section className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+      <Editor 
+        height="100%"
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+        theme="vs-dark"
+      />
+    </section>
 
-          {/* HEADING */}
-          <h1 className="max-w-2xl text-4xl font-semibold tracking-tight sm:text-5xl">
-            <AuroraText>
-              <code>fast-apps</code>
-            </AuroraText>
-          </h1>
-          <a
-            href="https://github.com/satendra03"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="cursor-pointer"
-          >
-            <code className="mt-2 mb-6 rounded bg-white/10 px-3 py-1 text-xs backdrop-blur">
-              by @satendra03
-            </code>
-          </a>
-        </main>
-    </div>
+   </main>
   );
 };
 
