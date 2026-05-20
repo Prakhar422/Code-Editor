3b461fd9fa5d5c0bc36f204a9e29aa42ee7a766d
Prakhar garg <gargprakhar422@gmail.com>
2026-05-20 17:38:14 +0530

    Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useMemo, useRef } from "react";
 import { AuroraText } from "./components/AuroraText";
 import { Editor } from "@monaco-editor/react";
 import {MonacoBinding} from "y-monaco";
-
+import * as Y from "yjs";
+import {SocketIOProvider} from "y-socket.io";
 
 const App = () => {
+
+  const editorRef = useRef(null)
+
+  const ydoc = useMemo(()=> new Y.Doc, [])
+  const yText = useMemo(()=> ydoc.getText("monaco"), [ydoc])
+
+  const handleMount = (editor) =>{
+    editorRef.current = editor
+
+    const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc,{
+      autoConnect: true
+    })
+    
+    const monaceBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    )
+  }
+
   return (
    <main className="h-screen w-full bg-gray-950 flex gap-4 p-2">
 
@@ -16,6 +38,7 @@ const App = () => {
         defaultLanguage="javascript"
         defaultValue="// some comment"
         theme="vs-dark"
+        onMount={handleMount}
       />
     </section>
 
